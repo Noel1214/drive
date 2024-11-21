6b28ed93ddeaac13f658818664f716dd82f02504
Noel Sebu <noelsebu555@gmail.com>
2024-11-21 22:53:38 +0530

    feat: configure express-validator and add validation for registration  data

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 /*  /user/  */
 
 router.get("/register", (req, res) => {
-    res.render("register")
+  res.render("register");
 });
 
-router.post("/get-user-data", (req, res) => {
+router.post(
+  "/get-user-data",
+
+  body("username").trim().isLength({ min: 3 }),
+  body("password").trim().isLength({ min: 3 }),
+  body("email").trim().isEmail(),
+
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log("found data validation errors");
+      res.send(errors);
+    }
     console.log(req.body);
-    res.redirect("/")
-})
+    res.redirect("/");
+  }
+);
 
 module.exports = router;
