b783a1949e1337380f13101505292e39eda1cdc5
Noel Sebu <noelsebu555@gmail.com>
2024-11-21 09:33:01 +0530

    feat: integrate user routes and add JSON and URL-encoded middleware

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
+const userRouter = require('./routes/user.routes');
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("index");
 });
+
+app.use('/user', userRouter);
 
 app.listen(3000, () => {
     console.log("server running on port 3000"); 
