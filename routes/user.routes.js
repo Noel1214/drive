a0a76424e44c9e021c64ab8f76e60a35c12c6f09
Noel Sebu <noelsebu555@gmail.com>
2024-11-22 19:10:18 +0530

    feat: implement user registration and improve the returning of error to client side

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const User = require("../models/user.model");
 
 /*  /user/  */
 
+// handles rendering of registration pagge
 router.get("/register", (req, res) => {
   res.render("register");
 });
 
+// handles registration of new user
 router.post(
   "/get-user-data",
 
@@ -15,13 +18,26 @@ router.post(
   body("password").trim().isLength({ min: 3 }),
   body("email").trim().isEmail(),
 
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      console.log("found data validation errors");
-      res.send(errors);
+      console.log("invalid data");
+      console.log(errors);
+      res.status(400).json({
+        errors: errors.array(),
+        message: "invalid data",
+      });
     }
-    console.log(req.body);
+    // console.log(req.body);
+    const { username, email, password } = req.body;
+    const newUser = new User({
+      username: username,
+      email: email,
+      password: password,
+    });
+    await newUser.save();
+
+    console.log("user created successfully");
     res.redirect("/");
   }
 );
