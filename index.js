56ee2fde1b2807afb9e7abd8737add284a5d013e
Noel Sebu <noelsebu555@gmail.com>
2024-11-22 19:02:02 +0530

    chore: initialize database connection for server

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const userRouter = require('./routes/user.routes');
+const connectDB = require('./config/db');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+connectDB();
 
 app.get("/", (req, res) => {
     res.render("index");
